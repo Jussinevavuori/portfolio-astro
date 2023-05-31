39fa93b3607cb194547809384fb813f007d61de5
Jussi Nevavuori <jussi@nevavuori.fi>
2023-05-31 22:16:08 +0300

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import vercel from "@astrojs/vercel/serverless";
+import image from "@astrojs/image";
+
+import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [tailwind(), sitemap(), prefetch(), image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	}), svelte()],
+	output: "static",
+	// output: "server",
+	// adapter: vercel()
+});
---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				"sans": `Poppins, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"`
+			},
+			colors: {
+				"primary": colors.green,
+			},
+			opacity: {
+				15: "15%"
+			}
+		},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+	],
+}
