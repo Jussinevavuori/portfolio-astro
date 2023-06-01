3601a984465d0bbdd8e7a8402e03d2d189a796df
Jussi Nevavuori <jussi@nevavuori.fi>
2023-06-01 15:23:40 +0300

    Attempt disable sharp

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind(), sitemap(), prefetch(), image({
-		serviceEntryPoint: '@astrojs/image/sharp'
+		// serviceEntryPoint: '@astrojs/image/sharp'
 	}), svelte()],
 	output: "server",
 	adapter: vercel()
